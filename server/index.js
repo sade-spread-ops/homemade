3adf19a16d5e48bbcd85a07a9eb82effcdb5c534
royce-reed <rjreed1@gmail.com>
2022-05-20 13:37:06 -0500

    add additional middleware

--- server/index.js
+++ server/index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const http = require('http');
 const helmet = require('helmet');
 const path = require('path');
-
+const morgan = require('morgan');
 
 const app = express();
-const PORT = 8701;
 
-
+// security - XSS protection and remove default headers
 app.use(helmet());
+// logs http req in terminal
+app.use(morgan('tiny'));
+
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
+const PORT = 8701;
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`
