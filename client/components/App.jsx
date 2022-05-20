5f9401c421f27791a5a33a892b8665e9c3200e4b
royce-reed <rjreed1@gmail.com>
2022-05-20 16:09:17 -0500

    refactor to react 18 and start with functional components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar.jsx';
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <h2>App Component</h2>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <NavBar />
+    <h2>App Component</h2>
+  </div>
+);
 
 export default App;
