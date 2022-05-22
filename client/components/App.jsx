3b5ff3c4cc843d8723415caae564d12d7e3316d2
royce-reed <rjreed1@gmail.com>
2022-05-21 19:59:07 -0500

    created nav bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import NavBar from './NavBar.jsx';
+import Navbar from './Navbar.jsx';
 import { Typography } from '@mui/material';
 const App = () => (
   <div>
-    <NavBar />
+    <Navbar />
     <Typography variant='h1'>
       App Component
     </Typography>
