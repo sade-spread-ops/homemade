23cfa576cfbed3d06df75d0da05e21ebaf369533
royce-reed <rjreed1@gmail.com>
2022-05-22 23:52:01 -0500

    create seed fn for db

---
+++ seeders/20220523041332-users-seeder.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const users = [];
+    for (let i = 0; i < 10; i++) {
+      users.push({
+        id: i++,
+        email: `foobar${i}@gmail.com`,
+        phone: `5049${i}65555`,
+        firstName: 'Foo',
+        lastName: 'Bar',
+        age: 18 + i,
+        gender: i % 2 === 0 ? 'Male' : 'Female',
+        lessee: i % 2 === 0,
+        lessor: i % 2 !== 0
+      });
+    }
+    return queryInterface.bulkInsert('users', users);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users', {}, null);
+  }
+};
