5276a9af714c452290d54db574392411dd0ea4f2
Raymond Jeong <raymondjjeong@gmail.com>
2022-05-23 17:22:37 -0500

    Created State Variables

---
+++ client/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Profile = () => {
+  const [googleId, setGoogleId] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState(18);
+  const [gender, setGender] = useState('');
+  const [lessee, setLessee] = useState(false);
+  const [lessor, setLessor] = useState(false);
+  const [location, setLocation] = useState('');
+  const [imageURL, setImageURL] = useState('');
+
+
+}
