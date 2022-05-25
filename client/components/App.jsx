c26d3b7e8888a419a485b8370fff4c55df8d537b
rysmith504 <rysmith504@gmail.com>
2022-05-25 16:30:33 -0500

    made feed component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -31,6 +31,7 @@ const App = () => {
         .catch((err) => console.error(err, '***ERROR***'));
     };
     getUser();
+    console.log(user);
   }, []);
   
   return (
@@ -40,10 +41,9 @@ const App = () => {
         ? <div>
           <Navbar />
           <Map />
-          <TinderCards />
         </div>
 
-        : <Typography align='center' ><a href={ `${CLIENT_URL}/auth/google` }>
+        : <Typography align='center' ><a href={ `${CLIENT_URL}}/auth/google` }>
           <Button variant="contained" color="primary" size='large' >
             Login with Google
           </Button></a></Typography>
