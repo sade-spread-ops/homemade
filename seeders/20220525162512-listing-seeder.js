ac5c167a037fda37c2c2c3aac5cd7dc4da28682a
royce-reed <rjreed1@gmail.com>
2022-05-25 23:45:35 -0500

    added seeder for listings table

--- seeders/20220525162512-listing-seeder.js
+++ seeders/20220525162512-listing-seeder.js
@@ -2,23 +2,19 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const listings = [];
-    for (let i = 0; i < 5; i++) {
-      listings.push({
-      //  id:
-      //  userId:
-      //  description:
-      //  imageURL:
-      //  longitude:
-      //  latitude:
-      //  address:
-      //  price:
-      });
-    }
-    return queryInterface.bulkInsert('Users', users);
+    return queryInterface.bulkInsert('listings', [{
+      id: 1,
+      userId: 1,
+      description: 'Lowes',
+      imageURL: 'https://lh5.googleusercontent.com/p/AF1QipMnFuhdJGF1fds7dtqgDW6mHevtiK6jh8JgK_bM=w426-h240-k-no',
+      longitude: -90.13098520876858,
+      latitude: 29.966469954768932,
+      address: '121 Jefferson Hwy, Jefferson, LA 70121',
+      price: 1200
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('listing', {}, null);
+    return queryInterface.bulkDelete('listings', {}, null);
   }
 };
