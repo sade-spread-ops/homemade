d5716ce3bb08bf5b65359eb74ed152238f31b6fc
royce-reed <rjreed1@gmail.com>
2022-05-26 12:31:28 -0500

    created frontend route and basic form to enter BASIC listing to DB

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,7 +1,8 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import { MAPBOX_TOKEN } from '../config/keys';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import axios from 'axios';
 // import { Typography } from '@mui/material';
 
 const Map = (props) => {
@@ -10,7 +11,33 @@ const Map = (props) => {
     latitude: 29.95,
     zoom: 12
   });
+  const [longitude, setLongitude] = useState(-90);
+  const [latitude, setLatitude] = useState(29);
+  const [listingsArr, setListingsArr] = useState();
+  console.log(props.user.id);
 
+  const createNewListing = () => {
+    axios.post('http://localhost:8000/listings', {
+      userId: props.user.id,
+      longitude: longitude,
+      latitude: latitude
+    });
+  };
+  // useEffect(() => {
+
+  const getAllListings = () => {
+    axios.get('http://localhost:8000/listings')
+      .then(listingsArr => {
+        // console.log(listingsArr.data);
+        setListingsArr(listingsArr.data);
+      })
+      .catch(err => console.error(err));
+  };
+  getAllListings();
+  // }, []);
+  console.log(listingsArr, 'ln38');
+
+  
   return ( 
     <div className="map">
       <ReactMapGL
@@ -25,10 +52,12 @@ const Map = (props) => {
         <Marker longitude={-90.15} latitude={29.966} color="red" />
       </ReactMapGL>
       <div className="add-listing">
+        
+
         <form>
-          <div><input type="text" placeholder='longitude'/></div>
-          <div><input type="text" placeholder='latitude'/></div>
-          <button>Create Marker</button>
+          <div><input type="number" step="0.0001" placeholder='longitude' onChange={e => setLongitude(e.target.value)} required/></div>
+          <div><input type="number" step="0.0001"placeholder='latitude' onChange={e => setLatitude(e.target.value)} required/></div>
+          <button onClick={() => createNewListing()}>Create Marker</button>
 
         </form>
       </div>
